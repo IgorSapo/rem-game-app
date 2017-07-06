c30d3bed861228d5dce7627fcc7b6df5dddb390d
IgorS <amsterdam1611@gmail.com>
2017-07-06 19:21:44 +0400

    Add login form

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,6 +4,7 @@ import NavigationBar from './NavigationBar';
 import Greetings from './Greetings';
 import SignUpPage from './signup/SignUpPage';
 import FlashMessagesList from './flash/FlashMessagesList';
+import LoginPage from './login/LoginPage';
 
 const App = ({ match }) => (
   <div className='container'>
@@ -11,6 +12,7 @@ const App = ({ match }) => (
     <FlashMessagesList />
     <Route exact path={'/'} component={Greetings} />
     <Route exact path={'/signup'} component={SignUpPage} />
+    <Route exact path={'/login'} component={LoginPage} />
   </div>
 )
 
