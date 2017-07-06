60e337ef97c3173d827f1a42a5eb5bde8ccde241
IgorS <amsterdam1611@gmail.com>
2017-07-06 16:26:45 +0400

    Validate username and email uniqueness on server

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,31 +1,65 @@
 import express from 'express';
-import validateInput from '../shared/validations/signup';
+import commonValidations from '../shared/validations/signup';
 import bcrypt from 'bcrypt';
+import Promise from 'bluebird';
+import isEmpty from 'lodash/isEmpty';
 
 import User from '../models/user';
 
 let router = express.Router();
 
+const validateInput = (data, otherValidations) => {
+  let { errors } = otherValidations(data);
+
+  return User
+    .query({
+      where: { email: data.email },
+      orWhere: { username: data.username }
+    })
+    .fetch()
+    .then(user => {
+      if(user) {
+        if(user.get('username') === data.username) {
+          errors.username = 'There is a user with this username';
+        }
+        if(user.get('email') === data.email) {
+          errors.email = 'There is a user with this email';
+        }
+      }
+    })
+    .then(() => {
+      return {
+        errors,
+        isValid: isEmpty(errors)
+      }
+    })
+}
+
 router.post('', (req, res) => {
-  const { errors, isValid } = validateInput(req.body);
-  if(isValid) {
-    const { username, password, timezone, email } = req.body;
-    const password_digest = bcrypt.hashSync(password, 10);
+  validateInput(req.body, commonValidations)
+    .then(({ errors, isValid }) => {
+      if(isValid) {
+        const { username, password, timezone, email } = req.body;
+        const password_digest = bcrypt.hashSync(password, 10);
 
-    User.forge({
-      username,
-      timezone,
-      email,
-      password_digest
-    }, {
-      hasTimestamps: true
-    }).save()
-      .then(user => res.json({ success: true }))
-      .catch(error => res.status(500).json({ error }));
+        User.forge(
+          {
+            username,
+            timezone,
+            email,
+            password_digest
+          },
+          {
+            hasTimestamps: true
+          }
+        ).save()
+         .then(user => res.json({ success: true }))
+         .catch(error => res.status(500).json({ error }));
 
-  } else {
-   res.status(400).json(errors);
-  }
+      } else {
+       res.status(400).json(errors);
+      }
+    });
 });
 
 export default router;
